b22f9f0a5b10b1f6df37e5e01b45e6a54e9603c2
Heidi St Jacques <heidi.stjacques@yahoo.com>
2025-07-20 13:23:51 -0400

    Create SuperuserReportsPage.jsx

---
+++ socialbright-frontend/src/tenants/tenant1/pages/Superuser/SuperuserReportsPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const SuperuserReportsPage = () => {
+  return (
+    <div>
+      <h1>Superuser Reports</h1>
+      {/* your content here */}
+    </div>
+  );
+};
+
+export default SuperuserReportsPage;
